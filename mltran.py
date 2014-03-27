3c448b973f83fcab384bc957bdd33a758acf27b9
Timofey Bondarev <bondarevts@gmail.com>
2014-03-28 00:29:43 +0400

    out to less

--- mltran.py
+++ mltran.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf8 -*-
 
 import sys
@@ -7,21 +7,43 @@ import requests
 from lxml import etree
 
 
+from subprocess import Popen, PIPE
+import errno
+
+
+def print_to_less(strings):
+    p = Popen('less', stdin=PIPE)
+    for s in strings:
+        try:
+            p.stdin.write(s)
+        except IOError as e:
+            if e.errno == errno.EPIPE or e.errno == errno.EINVAL:
+                # Stop loop on "Invalid pipe" or "Invalid argument".
+                # No sense in continuing with broken pipe.
+                break
+            else:
+                # Raise any other error.
+                raise
+
+    p.stdin.close()
+    p.wait()
+
+
 def make_request(word):
     request_address = 'http://www.multitran.ru/c/m.exe'
     response = requests.get(request_address, params={'s': word})
     print(response.url)
     code = etree.HTML(response.text.encode('cp1252').decode('cp1251'))
-    doc = etree.ElementTree(code)
-
-    result = etree.tostring(code, pretty_print=True, method="html", encoding='unicode')
+    # doc = etree.ElementTree(code)
+    #
+    # result = etree.tostring(code, pretty_print=True, method="html", encoding='unicode')
     # print(result)
 
     results_xpath = '/html/body/table/tr/td[2]/table/tr/td/table/tr[2]/td/table/tr/td[2]/table[2]'
 
-    for entry in code.xpath(results_xpath + '/tr/td[2]/a'):
-        print(entry.text)
-        # print(etree.tostring(entry, pretty_print=True, method="html", encoding='unicode'))
+    results = [entry.text.encode('utf-8') + '\n' for entry in code.xpath(results_xpath + '/tr/td[2]/a')]
+    print_to_less(results)
+    # print(etree.tostring(entry, pretty_print=True, method="html", encoding='unicode'))
 
 
 def main():
