423c53e0444920e4ed4dfad957b9d830223c06d7
Timofey Bondarev <bondarevts@gmail.com>
2014-03-30 01:52:11 +0400

    word differenciation, check part of speech, check translation category and pretty output

--- mltran.py
+++ mltran.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-import contextlib
 
 import sys
 import requests
+import contextlib
 
 from lxml import etree
 from subprocess import Popen, PIPE
+from collections import namedtuple
+
+
+def xml_element_to_string(elem):
+    return etree.tostring(elem, pretty_print=True, method="html", encoding='unicode')
 
 
 class LessPipe:
@@ -24,6 +29,12 @@ class LessPipe:
         self.p.wait()
 
 
+Translation = namedtuple('Translation', ['word', 'translation_items'])
+Translated = namedtuple('Translated', ['value', 'part_of_speech'])
+TranslationItem = namedtuple('TranslationItem', ['group', 'words'])
+Word = namedtuple('Word', ['value', 'context', 'comment', 'author'])
+
+
 class Mltran:
     def __init__(self, word, log=True, log_filename='unsorted_queries.txt'):
         if log:
@@ -39,19 +50,53 @@ class Mltran:
     def text(self):
         return self.response.text.encode('cp1252').decode('cp1251')
 
+    @staticmethod
+    def is_new_word_row(elem):
+        return elem.find('td[@bgcolor]') is not None
+
+    @staticmethod
+    def get_translated_from_row(row):
+        return Translated(row.find('td[@bgcolor]/a[1]').text, row.find('td[@bgcolor]/em').text.encode('utf-8'))
+
+    @staticmethod
+    def get_group(row):
+        return row.find('td[1]/a').get('title').encode('utf-8')
+
+    @staticmethod
+    def get_translations(row):
+        return [translation.text.encode('utf-8') for translation in row.findall('td[2]/a')]
+
     def results(self):
         code = etree.HTML(self.text())
         results_xpath = '/html/body/table/tr/td[2]/table/tr/td/table/tr[2]/td/table/tr/td[2]/table[2]'
-        return (entry.text.encode('utf-8') for entry in code.xpath(results_xpath + '/tr/td[2]/a'))
+
+        translated_word = None
+        translation_items = []
+        for row in code.xpath(results_xpath)[0].iterchildren():
+            if self.is_new_word_row(row):
+                if translated_word:
+                    yield Translation(translated_word, translation_items)
+                translation_items = []
+                translated_word = self.get_translated_from_row(row)
+            else:
+                translation_items.append(TranslationItem(self.get_group(row), self.get_translations(row)))
+        if translated_word:
+            yield Translation(translated_word, translation_items)
 
 
 def make_request(word):
-    request = Mltran(word)
+    request = Mltran(word, log=False)
     print(request.url())
 
     with contextlib.closing(LessPipe()) as less:
         less.write(request.url() + '\n')
-        less.writelines(request.results())
+        for result in request.results():
+            less.write('===== {}, {} =====\n'.format(result.word.value, result.word.part_of_speech))
+            for group in result.translation_items:
+                less.write('\tГруппа: {}\n'.format(group.group))
+                for translation in group.words:
+                    less.write(translation + '\n')
+                less.write('\n')
 
 
 def main():
